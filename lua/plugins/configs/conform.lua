97a05f76ba29e4c0fcf15f317f19ef0b9f2a473d
Enze Zhao <zhaoenze001@gmail.com>
2025-01-10 22:03:45 -0800

    Add use prettierd as the default formatter for ts and tsx

--- lua/plugins/configs/conform.lua
+++ lua/plugins/configs/conform.lua
@@ -5,8 +5,10 @@ opts = {
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "isort", "black" },
-		typescript = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettierd", "prettier" } },
+		go = { "gopls" },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { { "prettierd", "prettier", stop_after_first = true } },
+		typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 	},
