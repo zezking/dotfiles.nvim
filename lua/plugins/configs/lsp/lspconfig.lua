9cd62404c3f02ef4fd7d96569e70f5812ffbca71
Enze Zhao <zhaoenze001@gmail.com>
2025-01-13 12:35:07 -0800

    use gD to go to definition

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -25,7 +25,7 @@ local on_attach = function(client, bufnr)
 	end
 	-- set keybinds
 	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+	keymap.set("n", "gD", "<Cmd>Lspsaga goto_definition<CR>", opts) -- got to declaration
 	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
 	keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts) -- open diagnostics in float window
