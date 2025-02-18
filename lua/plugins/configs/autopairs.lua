a17590eb4e10f88963fb891d5b03c06b17869d79
Enze Zhao <zhaoenze001@gmail.com>
2025-02-18 08:30:15 -0800

    Better error handling in plugin init. Update lockfile

--- lua/plugins/configs/autopairs.lua
+++ lua/plugins/configs/autopairs.lua
@@ -1,5 +1,6 @@
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
+	print("nvim-autopairs not loaded")
 	return
 end
 
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,6 @@
 -- import nvim-treesitter plugin safely
-local status, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status then
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
 	return
 end
 
