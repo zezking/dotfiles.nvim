f83152991cd36098d189ec3269530c941c8cc2e0
Enze Zhao <zhaoenze001@gmail.com>
2026-05-20 22:48:10 -0700

    Remove copilot from the auto complete sources

--- lua/plugins/configs/lsp/cmp.lua
+++ lua/plugins/configs/lsp/cmp.lua
@@ -40,7 +40,7 @@ return {
 	-- Default list of enabled providers defined so that you can extend it
 	-- elsewhere in your config, without redefining it, due to `opts_extend`
 	sources = {
-		default = { "lsp", "copilot", "snippets", "buffer" },
+		default = { "lsp", "snippets", "buffer" },
 		providers = {
 			copilot = {
 				name = "copilot",
